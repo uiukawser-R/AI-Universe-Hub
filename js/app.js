a52219eaa5370278a14aa56ea5ea4020866883cf
uiukawser-R <kahmed171280@gmail.com>
2023-03-07 11:33:35 +0600

    update all

--- js/app.js
+++ js/app.js
@@ -1,40 +1,36 @@
- 
- 
- const loadAI_Universe_Hub= async (dataLimit)=>{
-    const url=`https://openapi.programming-hero.com/api/ai/tools`
-    const res=await fetch(url);
-    const data=await res.json();
+const loadAI_Universe_Hub = async (dataLimit) => {
+    const url = `https://openapi.programming-hero.com/api/ai/tools`
+    const res = await fetch(url);
+    const data = await res.json();
     // console.log(data.data.tools);
     // loading start 
     toggleSpinner(true);
-   
-     displayHub(data.data.tools,dataLimit)
-     
-     
+
+    displayHub(data.data.tools, dataLimit)
+
+
 }
+const displayHub = (hubs, dataLimit) => {
 
-
-const displayHub =(hubs,dataLimit)=>{
-    
     // console.log(hubs)
-        // display 6 hubs only 
-        const showAll = document.getElementById('show-all')
-        
-        if (dataLimit && hubs.length > 6) {
-            hubs = hubs.slice(0, 6);
-            showAll.classList.remove('d-none')
-    
-        }
-        else{
-            showAll.classList.add('d-none')
-        }
-    const hubContainer=document.getElementById('hub-container');
+    // display 6 hubs only 
+    const showAll = document.getElementById('show-all')
+
+    if (dataLimit && hubs.length > 6) {
+        hubs = hubs.slice(0, 6);
+        showAll.classList.remove('d-none')
+
+    }
+    else {
+        showAll.classList.add('d-none')
+    }
+    const hubContainer = document.getElementById('hub-container');
     hubContainer.innerHTML = '';
     hubs.forEach(hub => {
         // console.log(hub)
-        const hubDiv=document.createElement('div');
+        const hubDiv = document.createElement('div');
         hubDiv.classList.add('col')
-        hubDiv.innerHTML=`
+        hubDiv.innerHTML = `
         <div class="card">
         <img src="${hub.image}" style="height: 300px;" class="card-img-top p-2" alt="...">
         <div class="card-body">
@@ -64,76 +60,76 @@ const displayHub =(hubs,dataLimit)=>{
       </div>
         `
         hubContainer.appendChild(hubDiv);
-        
-        
+
+
     });
 
-    
+
     // loading stop 
     toggleSpinner(false);
 }
 
 // loder 
-const toggleSpinner=isLoding=>{
-    const loaderSection=document.getElementById('loder')
-    if(isLoding){
+const toggleSpinner = isLoding => {
+    const loaderSection = document.getElementById('loder')
+    if (isLoding) {
         loaderSection.classList.remove('d-none');
     }
-    else{
+    else {
         loaderSection.classList.add('d-none');
     }
 }
 
-document.getElementById('btn-show-all').addEventListener('click',function(){
+document.getElementById('btn-show-all').addEventListener('click', function () {
     loadAI_Universe_Hub();
 })
 loadAI_Universe_Hub(6);
 
 
 
-const loadHubDetails=async id =>{
-    const url=`https://openapi.programming-hero.com/api/ai/tool/${id}`
+const loadHubDetails = async id => {
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`
     const res = await fetch(url)
-    const data= await res.json()
+    const data = await res.json()
     // console.log(data.data);
     displayHubDetails(data.data)
 }
 
-const displayHubDetails=Hub=>{
+const displayHubDetails = Hub => {
     console.log(Hub);
-    const HubDescription=document.getElementById('hub-description')
-    HubDescription.innerText=Hub.description
-    document.getElementById('p1').innerText=Hub?.pricing?.[0].price? Hub.pricing[0].price: "hello"
-    document.getElementById('p2').innerText=Hub?.pricing?.[0].plan ? Hub.pricing[0].plan : "hello"
+    const HubDescription = document.getElementById('hub-description')
+    HubDescription.innerText = Hub.description
+    document.getElementById('p1').innerText = Hub?.pricing?.[0].price ? Hub.pricing[0].price : "hello"
+    document.getElementById('p2').innerText = Hub?.pricing?.[0].plan ? Hub.pricing[0].plan : "hello"
 
-    document.getElementById('p3').innerText=Hub?.pricing?.[1].price? Hub.pricing[1].price: "hello"
-    document.getElementById('p4').innerText=Hub?.pricing?.[1].plan ? Hub.pricing[1].plan : "hello"
+    document.getElementById('p3').innerText = Hub?.pricing?.[1].price ? Hub.pricing[1].price : "hello"
+    document.getElementById('p4').innerText = Hub?.pricing?.[1].plan ? Hub.pricing[1].plan : "hello"
 
-    document.getElementById('p5').innerText=Hub?.pricing?.[2].price? Hub.pricing[2].price: "hello"
-    document.getElementById('p6').innerText=Hub?.pricing?.[2].plan ? Hub.pricing[2].plan : "hello"
+    document.getElementById('p5').innerText = Hub?.pricing?.[2].price ? Hub.pricing[2].price : "hello"
+    document.getElementById('p6').innerText = Hub?.pricing?.[2].plan ? Hub.pricing[2].plan : "hello"
 
-    document.getElementById('l1').innerText=Hub.features[1].feature_name
-    document.getElementById('l2').innerText=Hub.features[2].feature_name
-    document.getElementById('l3').innerText=Hub.features[3].feature_name
-    
-    document.getElementById('l4').innerText=Hub?.integrations[0]?Hub.integrations[0]:"no Data"
-    document.getElementById('l5').innerText=Hub?.integrations[1]?Hub.integrations[1]:"no Data"
-    document.getElementById('l6').innerText=Hub?.integrations[2]?Hub.integrations[2]:"no Data"
+    document.getElementById('l1').innerText = Hub.features[1].feature_name
+    document.getElementById('l2').innerText = Hub.features[2].feature_name
+    document.getElementById('l3').innerText = Hub.features[3].feature_name
+
+    document.getElementById('l4').innerText = Hub?.integrations[0] ? Hub.integrations[0] : "no Data"
+    document.getElementById('l5').innerText = Hub?.integrations[1] ? Hub.integrations[1] : "no Data"
+    document.getElementById('l6').innerText = Hub?.integrations[2] ? Hub.integrations[2] : "no Data"
 
 
-    document.getElementById('card-body').innerHTML= `
+    document.getElementById('card-body').innerHTML = `
     <div class="text-center">
                                         <img src="${Hub.image_link[0]}" class="img-fluid" alt="" srcset="">
                                         <h5 class="pt-3">${Hub.input_output_examples[0].input}</h5>
                                         <div class="text-center rounded p-1"
                                             style="position:relative; top: -310px; right: -220px; background-color: red; color: white; height: 32px; width: 140px;">
-                                            ${Hub.accuracy?.score?Hub.accuracy.score*100 : "no data"}% accuracy
+                                            ${Hub.accuracy?.score ? Hub.accuracy.score * 100 : "no data"}% accuracy
                                         </div>
                                         <p>${Hub.input_output_examples[0].output}</p>
                                     </div>
     `
-  
-    
+
+
 
 }
 
